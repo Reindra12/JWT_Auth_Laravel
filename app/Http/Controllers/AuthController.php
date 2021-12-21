69ebe2f853979c2c7ee12c602fcbd3e2cb813f2b
Reindra12 <reindrairawan@gmail.com>
2021-12-21 20:13:49 +0700

    validasi untuk session login driver

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,8 +5,9 @@ use Illuminate\Http\Request;
 
 use Illuminate\Support\Facades\Auth;
 use App\Models\Driver;
+use App\Models\Petugas;
 use Validator;
-
+use DB;
 
 
 class AuthController extends Controller
@@ -17,7 +18,7 @@ class AuthController extends Controller
      * @return void
      */
     public function __construct() {
-        $this->middleware('auth:api', ['except' => ['login', 'register']]);
+        $this->middleware('auth:api', ['except' => ['login', 'register','registerpetugas']]);
     }
 
     /**
@@ -25,11 +26,25 @@ class AuthController extends Controller
      *
      * @return \Illuminate\Http\JsonResponse
      */
+
+
     public function login(Request $request){
+
+    // $rules = [
+    //     'user' => 'required|email',
+    //     'password' => 'required|string|min:6',
+    // ];
+    // $messages = [
+    // "user.exists" => "Lorem ipsum dolor sit amet.",
+    // "password.unique" => "Lorem ipsum dolor sit amet."
+    // ];
+
     	$validator = Validator::make($request->all(), [
-            'User' => 'required|email',
+            'user' => 'required|email',
             'password' => 'required|string|min:6',
         ]);
+        
+        // $validator = Validator::make($request->all(), $rules, $messages);
 
         if ($validator->fails()) {
             return response()->json($validator->errors(), 422);
@@ -37,7 +52,17 @@ class AuthController extends Controller
 
         if (! $token = auth()->attempt($validator->validated())) {
             return response()->json(['error' => 'Unauthorized'], 401);
+        if(auth()->guard('api2')->attempt($validator->validated())){
+
+            config(['auth.guards.api.provider' => 'api2']);
+            
+            $petugas = Petugas::select('tb_petugas.*')->find(auth()->guard('api2')->user()->id_petugas);
+            $success =  $petugas;
+            // $success['token'] =  $petugas->createToken('MyApp',['admin'])->accessToken; 
+            
+            return response()->json($success, 200);
         }
+    
 
         return $this->createNewToken($token);
     }
@@ -49,9 +74,9 @@ class AuthController extends Controller
      */
     public function register(Request $request) {
         $validator = Validator::make($request->all(), [
-            'id_driver' => 'required|string|between:1,100',
+            // 'id_driver' => 'required|string|between:1,100',
             'nama_driver' => 'required|string|between:2,100',
-            'User' => 'required|string|email|max:100|unique:tb_driver',
+            'user' => 'required|string|email|max:100|unique:tb_driver',
             'password' => 'required|string|confirmed|min:6',
             'no_ktp' => 'required|string|between:5,100',
 
@@ -62,6 +87,30 @@ class AuthController extends Controller
         }
 
         $user = Driver::create(array_merge(
+                    $validator->validated(),
+                    ['password' => bcrypt($request->password)]
+                ));
+
+        return response()->json([
+            'message' => 'User successfully registered',
+            'user' => $user
+        ], 201);
+    }
+    public function registerpetugas(Request $request) {
+        $validator = Validator::make($request->all(), [
+            // 'id_driver' => 'required|string|between:1,100',
+            'nama_lengkap' => 'required|string|between:2,100',
+            'user' => 'required|string|email|max:100|unique:tb_petugas',
+            'password' => 'required|string|confirmed|min:6',
+            // 'no_ktp' => 'required|string|between:5,100',
+
+        ]);
+
+        if($validator->fails()){
+            return response()->json($validator->errors()->toJson(), 400);
+        }
+
+        $user = Petugas::create(array_merge(
                     $validator->validated(),
                     ['password' => bcrypt($request->password)]
                 ));
@@ -114,7 +163,9 @@ class AuthController extends Controller
             'access_token' => $token,
             'token_type' => 'bearer',
             'expires_in' => auth()->factory()->getTTL() * 60,
-            'user' => auth()->user()
+            'user' => auth()->user(),
+        
+            'message' => 'User successfully registered',
         ]);
     }
 
